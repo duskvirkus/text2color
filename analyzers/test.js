a0d1e5c2e48aae5656ae1c0c97881ee4fa2a165a
figraham <fi.sage.graham@gmail.com>
2018-12-03 04:01:24 -0700

    deleted test analyzer

--- analyzers/test.js
+++
@@ -1,8 +0,0 @@
-testAnalyzer = s => {
-  colorMode(RGB, 255);
-  if (s.length == 0) {
-    return color(255);
-  } else {
-    return s.length < 11 ? color(0, 0, 255 - s.length * 25) : color(0);
-  }
-}
